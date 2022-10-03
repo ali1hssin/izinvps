9ae9c90c397e90d6ac6d3c479f63e5c868741348
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-10-03 17:51:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,16 +7,17 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
+TEST='\E[41;1;39m'
 
 clear
-echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[41;1;39m             ⇱ MENU TOOLS INSTALLER ⇲                            \E[0m"
-echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "[${GREEN}01${NC}] ${PURPLE}👉${NC} Install Script V.1 ( ${GREEN}MultiPort${NC} )"
-echo -e "[${GREEN}02${NC}] ${PURPLE}👉${NC} Install Script V.2 ( ${GREEN}No MultiPort${NC} )"
-echo -e "[${GREEN}03${NC}] ${PURPLE}👉${NC} Izin IP VPS Ke Admin ( ${GREEN}RIDWAN${NC} )"
-echo -e "[${GREEN}00${NC}] ${PURPLE}❌${NC} Exit"
-echo -e "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo "${TEST}             ⇱ MENU TOOLS INSTALLER ⇲                            \E[0m${NC}"
+echo "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo "[${GREEN}01${NC}] ${PURPLE}👉${NC} Install Script V.1 ( ${GREEN}MultiPort${NC} )"
+echo "[${GREEN}02${NC}] ${PURPLE}👉${NC} Install Script V.2 ( ${GREEN}No MultiPort${NC} )"
+echo "[${GREEN}03${NC}] ${PURPLE}👉${NC} Izin IP VPS Ke Admin ( ${GREEN}RIDWAN${NC} )"
+echo "[${GREEN}00${NC}] ${PURPLE}❌${NC} Exit"
+echo "${GREEN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo ""
 echo -ne "Select menu : "; read x
 
@@ -33,7 +34,7 @@ case "$x" in
    
    3 | 03)
    clear
-   https://t.me/r1f4n_112
+   read -n 1 -s -r -p "Chat Tele Admin @r1f4n_112"
    ;;
    *)
    xdrg
